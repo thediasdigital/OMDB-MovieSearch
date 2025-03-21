5c818e862151b2126035190e54274d1c595a8351
Bennett Dias <bennett.dias3@gmail.com>
2025-03-21 16:29:23 -0400

    Movie model

---
+++ app/src/main/java/com/example/omdb_moviesearch/model/Movie.java
@@ -0,0 +1,87 @@
+package com.example.omdb_moviesearch.model;
+
+public class Movie {
+    private String imbdID;
+    private String title;
+    private String year;
+    private String rating;
+    private String runtime;
+    private String genre;
+    private String imdbRating;
+    private String plot;
+    private String posterUrl;
+
+    public Movie() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getImbdID() {
+        return imbdID;
+    }
+
+    public void setImbdID(String imbdID) {
+        this.imbdID = imbdID;
+    }
+}
