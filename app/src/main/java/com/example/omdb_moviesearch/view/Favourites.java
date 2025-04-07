4f8d34db6641506ab4510f1be2bd8c0e26a4cca0
Bennett Dias <bennett.dias3@gmail.com>
2025-04-07 13:40:06 -0400

    Removed google-services from tracking in repo

---
+++ app/src/main/java/com/example/omdb_moviesearch/view/Favourites.java
@@ -0,0 +1,26 @@
+package com.example.omdb_moviesearch.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.omdb_moviesearch.R;
+
+public class Favourites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_favourites);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
